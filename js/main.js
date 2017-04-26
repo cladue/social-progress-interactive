c7b220743e9a02351abc9c48a68b503bbd2c08f2
sekamajor <sekamajor@gmail.com>
2017-04-25 19:37:36 -0500

    Got rid of my typojson and added shit to our java

--- js/main.js
+++ js/main.js
@@ -29,6 +29,31 @@ function setMap(){
     function callback(error, csvData, world){
         //translate TopoJSON
         var worldCountries = topojson.feature(world, world.objects.collection).features;
+        //variables for data join
+        var attrArray = ["Country","Social_Progress_Index","Rank_(BHN)","Basic_Human_Needs","Rank_(FW)",
+        "Foundations_of_Well-Being","Rank_(O)","Opportunity"];
+        //loop through csv to assign each set of csv attribute values to geojson region
+        for (var i=0; i<csvData.length; i++){
+        var csvRegion = csvData[i]; //the current region
+        var csvKey = csvRegion.adm0_a3; //the CSV primary key
+
+        //loop through geojson regions to find correct region
+        for (var a=0; a<worldCountries.length; a++){
+
+            var geojsonProps = worldCountries[a].properties; //the current region geojson properties
+            var geojsonKey = geojsonProps.adm0_a3; //the geojson primary key
+
+            //where primary keys match, transfer csv data to geojson properties object
+            if (geojsonKey == csvKey){
+
+                //assign all attributes and values
+                attrArray.forEach(function(attr){
+                    var val = parseFloat(csvRegion[attr]); //get csv attribute value
+                    geojsonProps[attr] = val; //assign attribute and value to geojson properties
+                });
+            };
+        };
+    };
 
         //add countries to map
         var countries = map.selectAll(".countries")
